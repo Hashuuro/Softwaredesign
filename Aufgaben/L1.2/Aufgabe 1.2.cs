ba85d07db47f44bd4435e9e61030f1dabd315d59
Hashuuro <Lukas_Walther@gmx.de>
2018-04-03 16:01:25 +0200

    Anpassung und Verbesserung

--- Aufgaben/L1.2/Aufgabe 1.2.cs
+++ Aufgaben/L1.2/Aufgabe 1.2.cs
@@ -6,57 +6,58 @@ namespace L1._2
     {
         static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
         static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
-        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" }; 
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
         static string sub;
         static string verb;
         static string obj;
-                static void Main(string[] args)
+        static void Main(string[] args)
         {
-           string[] verse = new string[subjects.Length];
+            string[] zwischenspeicher = new string[subjects.Length];
+
             for (int i = 0; i < subjects.Length; i++)
             {
-                getVerse();
-                verse[i] = sub + " " + verb + " " + obj;
+                getVers();
+
+                zwischenspeicher[i] = sub + " " + verb + " " + obj;
+                Console.WriteLine(zwischenspeicher[i]);
             }
-            for (int i = 0; i < subjects.Length; i++){
-                Console.WriteLine(verse[i]);
-}
-            
+        
         }
 
-      public static void getVerse(){
+        public static void getVers()
+        {
 
             Random ran = new Random();
 
             int s = ran.Next(0, subjects.Length);
             int v = ran.Next(0, verbs.Length);
             int o = ran.Next(0, objects.Length);
-
-           while (subjects[s] == "used")
+       
+            while (subjects[s] == "vergeben")
             {
                 s = ran.Next(0, subjects.Length);
             }
             sub = subjects[s];
-            subjects[s] = "used";
+            subjects[s] = "vergeben";
 
-            while (verbs[v] == "used")
+            while (verbs[v] == "vergeben")
             {
                 v = ran.Next(0, subjects.Length);
             }
             verb = verbs[v];
-            verbs[v] = "used";
+            verbs[v] = "vergeben";
 
-            while (objects[o] == "used")
+            while (objects[o] == "vergeben")
             {
                 o = ran.Next(0, subjects.Length);
             }
             obj = objects[o];
-            objects[o] = "used";
-        
-      }
+            objects[o] = "vergeben";
+
+        }
     }
 
 
-        
-    
+
+
 }
