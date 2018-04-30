90d79df52893748f9290ad862827f58f10ee4ba4
Hashuuro <Lukas_Walther@gmx.de>
2018-04-30 19:53:26 +0200

    cleanup

--- Aufgaben/L1/Aufgabe 1.1.cs
+++ Aufgaben/L1/Aufgabe 1.1.cs
@@ -7,87 +7,89 @@ namespace L1
         static void Main(string[] args)
         {
 
-        string w = "w";// müssen nicht definiert werden!
-        string k = "k";
-        string o = "o";
-        double result;
+
+            double result;
 
 
-        if (args.Length == 0)// Lieber switch case, einfacher zu lesen, args[0] als Variable, damit Programm nicht immer in Array muss
-        {
-            System.Console.WriteLine("Bitte das Kürzel für den zu berechnenden Körper eingeben. w=Würfel, k=Kugel, o=Oktaeder.");
-            System.Console.WriteLine("Danach den Wert für die Kantenlänge bzw. den Durchmesser. Bsp: dotnet run w 2,34");
+            if (args.Length == 0)// Lieber switch case, einfacher zu lesen, args[0] als Variable, damit Programm nicht immer in Array muss
+            {
+                System.Console.WriteLine("Bitte das Kürzel für den zu berechnenden Körper eingeben. w=Würfel, k=Kugel, o=Oktaeder.");
+                System.Console.WriteLine("Danach den Wert für die Kantenlänge bzw. den Durchmesser. Bsp: dotnet run w 2,34");
+            }
+            else if (args[0] == "w")
+            {
+
+                string character = args[1];
+                result = Double.Parse(character);
+                getcubeInfo(result);
+            }
+            else if (args[0] == "k")
+            {
+
+                string character = args[1];
+                result = Double.Parse(character);
+                getballInfo(result);
+
+            }
+            else if (args[0] == "o")
+            {
+
+                string character = args[1];
+                result = Double.Parse(character);
+                getOktaederInfo(result);
+
+            }
+            else
+            {
+
+                System.Console.WriteLine("Bitte gültiges Kürzel eingeben.");
+            }
+
+
         }
-        else if (args[0]==w){
 
-            string v = args[1];
-            result = Double.Parse(v);
-            getKubusInfo(result);
-        }
-        else if(args[0]==k){
-
-            string v = args[1];
-            result = Double.Parse(v);
-            getKugelInfo(result);
-
-        }
-        else if(args[0]==o){
-            
-            string v = args[1];
-            result = Double.Parse(v);
-            getOktaederInfo(result);
-
-        }
-        else{
-
-            System.Console.WriteLine("Bitte gültiges Kürzel eingeben.");
-        }
-
-          
-        }
-
-        public static double Kubusflaeche(double d) // Variabeln und Code auf Englisch. Zur Übersicht region info!
+        public static double cubearea(double d) // Variabeln und Code auf Englisch. Zur Übersicht region info!
         {
             double A = Math.Pow((6 * d), 2);
             return A;
         }
-        public static double KubusVolumen(double d)
+        public static double cubeVolume(double d)
         {
             double V = Math.Pow(d, 3);
             return V;
         }
 
-        public static void getKubusInfo(double d) // keine Rückgabe, obwohl in Aufgabenstellung vorgegeben
+        public static void getcubeInfo(double d) // keine Rückgabe, obwohl in Aufgabenstellung vorgegeben
         {
-            double a = Math.Round(Kubusflaeche(d),2);
-            double b = Math.Round(KubusVolumen(d),2);
-            Console.WriteLine("Würfelfläche A: " + a + " Würfelvolumen V: " + b);
+            double areac = Math.Round(cubearea(d), 2);
+            double volumec = Math.Round(cubeVolume(d), 2);
+            Console.WriteLine("Würfelfläche A: " + areac + " Würfelvolumen V: " + volumec);
         }
-        public static double Kugelflaeche(double d)
+        public static double ballarea(double d)
         {
             double A = Math.Pow((Math.PI * d), 2);
             return A;
         }
-        public static double KugelVolumen(double d)
+        public static double ballVolume(double d)
         {
             double V = (Math.Pow(Math.PI * d, 3)) / 6;
             return V;
         }
 
-        public static void getKugelInfo(double d) // keine Rückgabe, obwohl in Aufgabenstellung vorgegeben
+        public static void getballInfo(double d) // keine Rückgabe, obwohl in Aufgabenstellung vorgegeben
         {
-             double a = Math.Round(Kugelflaeche(d),2);
-            double b = Math.Round(KugelVolumen(d),2);
-            Console.WriteLine("Kugelfläche A: " + a + " Kugelvolumen V: " + b);
+            double areaB = Math.Round(ballarea(d), 2);
+            double volumeB = Math.Round(ballVolume(d), 2);
+            Console.WriteLine("Kugelfläche A: " + areaB + " Kugelvolumen V: " + volumeB);
         }
-        public static double Oktaederflaeche(double d)
+        public static double Oktaederarea(double d)
         {
             double a = 2 * Math.Sqrt(3);
             double b = Math.Pow(d, 2);
             double A = a * b;
             return A;
         }
-        public static double OktaederVolumen(double d)
+        public static double OktaederVolume(double d)
         {
             double a = Math.Sqrt(2);
             double b = Math.Pow(d, 3);
@@ -98,9 +100,9 @@ namespace L1
 
         public static void getOktaederInfo(double d) // keine Rückgabe, obwohl in Aufgabenstellung vorgegeben
         {
-            double a = Math.Round(Oktaederflaeche(d),2);
-            double b = Math.Round(OktaederVolumen(d),2);
-            Console.WriteLine("Oktaederfläche A: " + a + " Oktaedervolumen V: " + b);
+            double areaO = Math.Round(Oktaederarea(d), 2);
+            double volumeO = Math.Round(OktaederVolume(d), 2);
+            Console.WriteLine("Oktaederfläche A: " + areaO + " Oktaedervolumen V: " + volumeO);
         }
     }
 }
