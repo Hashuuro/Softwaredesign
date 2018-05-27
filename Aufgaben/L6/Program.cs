c6aaee198bf2085d35caef19f77ea5827a628c0f
Hashuuro <Lukas_Walther@gmx.de>
2018-05-27 17:00:10 +0200

    Aufgabe L6 fertig

---
+++ Aufgaben/L6/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L6
+{
+
+
+    class Person
+    {
+        public string name;
+        public int age;
+    }
+
+    class Member : Person
+    {
+        public int matriculationnumber;
+        public List<Course> Courses;
+    }
+
+    class Lecturer : Person
+    {
+        public string office;
+        public string consultation;
+
+        public List<Course> Courses;
+
+        public void courselist()
+        {
+            foreach (Course Course in Courses)
+                Console.WriteLine(Course.title);
+        }
+
+        public List<Member> memberlist()
+        {
+            List<Member> allmembers = new List<Member>();
+
+            foreach (Course Course in Courses)
+            {
+                foreach (Member member in Course.Members)
+                {
+                    if (!allmembers.Contains(member))
+                    {
+                        allmembers.Add(member);
+                    }
+                }
+            }
+            return allmembers;
+        }
+    }
+
+    class Course
+    {
+        public string title;
+        public string room;
+        public string weekday;
+        public string timeofday;
+
+        public Lecturer Lecturer;
+        public List<Member> Members;
+
+        public void infotext()
+        {
+            Console.WriteLine("Der Kurs " + title + " findet am " + weekday + "um" + timeofday + " in Raum " + room + " statt.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Aufgaben/L6_UML/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace L6_UML
+{
+
+
+    public interface Fahrzeug
+    {
+        void Fahrlos(double MaxSpeed);
+    }
+    public class BMW : Fahrzeug
+    {
+        public string Farbe;
+        public int  Zylinder;
+        public void Fahrlos(double MaxSpeed)
+        {
+            if(MaxSpeed>=100){
+                Console.WriteLine("Blitzer!!");
+            }
+            else{
+                Console.WriteLine("toller Fahrer");
+            }
+        } 
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var auto = new BMW();
+        auto.Zylinder = 4;
+        auto.Farbe = "grün";
+        auto.Fahrlos(100);
+       
+        }
+    }
+}
