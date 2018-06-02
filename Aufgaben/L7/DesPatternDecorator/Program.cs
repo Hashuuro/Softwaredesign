074b5b1f5ac162a1296593ed3061011c4dfd2e16
Hashuuro <Lukas_Walther@gmx.de>
2018-06-02 18:00:21 +0200

    L7 finished

---
+++ Aufgaben/L7/DesPatternDecorator/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesPatternDecorator
+{
+    interface Spielfigur
+    {
+        void Drohe();  
+    }
+
+    class Monster : Spielfigur
+    {
+        public void Drohe()
+        {
+            Console.Write("Grrrrr!");
+        }
+    } 
+
+
+    class Held : Spielfigur
+    {
+        public void Drohe()
+        {
+            Console.Write("Weiche zurück!");
+        }        
+    }
+
+
+    // Decorator
+    class Basis : Spielfigur
+    {
+        protected Spielfigur _original;
+
+        public Basis(Spielfigur original)
+        {
+            _original = original; 
+        }
+
+        virtual public void Drohe()
+        {
+            _original.Drohe();
+        }
+}
+   class ErkaelteteFigur : Basis
+    {
+        public ErkaelteteFigur(Spielfigur original) : base(original){}
+
+        override public void Drohe()
+        {
+            _original.Drohe();
+            Console.Write(" Hust!");
+        }
+}
+
+    class HeisereFigur : Basis
+    {
+        public HeisereFigur(Spielfigur original) : base(original){}
+
+        override public void Drohe()
+        {
+            Console.Write("Räusper...");
+            _original.Drohe();
+        }
+}
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Spielfigur> figuren = new List<Spielfigur>();
+
+            figuren.Add(new Monster());
+            figuren.Add(new Held());
+            figuren.Add(new ErkaelteteFigur(new Held()));
+            figuren.Add(new ErkaelteteFigur(new ErkaelteteFigur(new Monster())));
+            figuren.Add(new HeisereFigur(new Monster()));
+            figuren.Add(new ErkaelteteFigur(new HeisereFigur(new Held())));
+
+            foreach(var spielfigur in figuren)
+            {
+                spielfigur.Drohe();
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- Aufgaben/L7/DesPatternSingleton/Program.cs
+++ Aufgaben/L7/DesPatternSingleton/Program.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-namespace L07
+
+namespace DesPatternSingleton
 {
 
-    
-        public class Person
+    public class Person
     {
         public string Name;
         public int Age;
-        private int Id;
-
-       public static List<Person> personen = new List<Person>();
-        public Person(string Name, int Age)
-        {
-            this.Name = Name;
-            this.Age = Age;
-            this.Id = IDGenerator.GetInstance().GibMirNeId();
-            personen.Add(this);
-        }
+        public int Id;
 
         public override string ToString()
         {
             return "Name:" + Name + ", Age: " + Age + ", " + "Id: " + Id;
         }
-        
-        
+
+         public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Id = IDGenerator.Instance.GibMirNeId();
+            Program.personen.Add(this);
+        }
     }
 
     /*
@@ -44,35 +41,35 @@ namespace L07
 
         private static IDGenerator _instance;
 
-        public static IDGenerator GetInstance()
+       public static IDGenerator Instance
         {
-            if (_instance == null)
-                _instance = new IDGenerator();
-            return _instance;
+            get
+            {
+                if (_instance == null)
+                    _instance = new IDGenerator();
+                return _instance;
+            }
         }
-
         private int letzteID;
         public int GibMirNeId()
         {
             return letzteID++;
         }
-
     } 
 
     class Program
     {
-
+        public static List<Person> personen = new List<Person>();
         static void Main(string[] args)
         {
             
-
-            
+           
 
             // Eine Stelle, an der Personen angelegt werden
             new Person("Dieter", 44);
-            new Person("Horst",45);
+            new Person("Horst", 45);
             new Person("Walter", 33);
-            new Person( "Karl-Heinz", 22);
+            new Person("Karl-Heinz", 22);
 
 
             // Eine ANDERE Stelle, an der Personen angelegt werden
@@ -80,14 +77,13 @@ namespace L07
             new Person("Maria", 75);
             new Person("Kunigunde", 22);
             new Person("Tusnelda", 12);
-             
-             //protected-----> super und sub klassen haben Zugriff
-             //constructor----> methode mit gleichem namen wie klasse 
 
-            
 
-            foreach (var person in Person.personen)
-             Console.WriteLine(person);
+
+
+
+            foreach (var person in personen)
+                Console.WriteLine(person);
 
             Console.WriteLine("Hello World!");
         }
