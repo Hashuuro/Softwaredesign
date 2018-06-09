619ec3e4a15605f3b89d503aec0b2010a40c5929
Hashuuro <Lukas_Walther@gmx.de>
2018-06-09 19:23:02 +0200

    Diagramm fertig

---
+++ Aufgaben/L8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace L8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Player1 Eingabe:");
+           String a = Console.ReadLine();
+    
+            Console.WriteLine(" - "+"- "+"- ");
+            Console.WriteLine("| "+a+" "+" "+" |");
+            Console.WriteLine("| "+a+" "+" "+" |");
+            Console.WriteLine("| "+a+" "+" "+" |");
+            Console.WriteLine(" - "+"- "+"- ");
+        }
+    }
+}
